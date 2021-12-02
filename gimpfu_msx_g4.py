523b84a65583ebaae4d1b101a25bd3d65cb14d5a
Pedro de Medeiros <pedro.medeiros@gmail.com>
2021-12-02 16:50:49 -0300

    better color reducing

--- gimpfu_msx_g4.py
+++ gimpfu_msx_g4.py
@@ -15,7 +15,7 @@ import gimp
 import os
 import struct
 
-FILE_PREFIX = 0xFE
+BIN_PREFIX = 0xFE
 DEFAULT_FILENAME = 'NONAME'
 DEFAULT_VRES = 'v212'
 DEFAULT_OUTPUT_DIR = os.getcwd()
@@ -26,26 +26,21 @@ MAX_HEIGHT = 212
 MAX_PAGES = 4
 PALETTE_OFFSET = 0x7680
 
-def write_gr4(img, layer, filename, folder, layers2pages, palette, image_enc):
+def write_gr4(img, layer, filename, folder, black0, exp_pal, image_enc):
     '''
-    Export image to GRAPHICS 4 (MSX2).
+    Export image to GRAPHICS 4, a.k.a. SCREEN 5 (MSX2).
     
     @param img: gimp image
     @param layer: gimp layer (or drawable)
     @param filename: file name
     @param folder: output directory
-    @param layers2pages: convert extra layers into pages
-    @param palette: write palette data too
+    @param black0: force black as color 0
+    @param exp_pal: export palette data too
     @param image_enc: output encoding
     '''
     
     errors = []
     drawable = gimpfu.pdb.gimp_image_active_drawable(img)
-    num_bytes, cmap = gimpfu.pdb.gimp_image_get_cmap(img)
-    colors = []
-
-    if num_bytes // 3 > MAX_COLORS:
-        errors.append('Image must not have more than 16 colors.')
 
     width, height = gimpfu.pdb.gimp_drawable_width(drawable), gimpfu.pdb.gimp_drawable_height(drawable)
 
@@ -62,49 +57,79 @@ def write_gr4(img, layer, filename, folder, layers2pages, palette, image_enc):
         gimp.message("\n".join(errors))
         return
 
-    gimpfu.pdb.gimp_progress_init('Converting to 9-bit colors...', None)
+    # Saving palette
+    cfq, crep = reduce_colors(img)
+
+    if len(cfq) > MAX_COLORS:
+        errors.append('Image must not have more than 16 colors.')
+
+    gimpfu.pdb.gimp_progress_init('Converting to %i colors...' % len(cfq), None)
     gimpfu.pdb.gimp_progress_update(0)
 
-    # Saving palette
-    if palette:
-        colorsz = num_bytes // 3 * 2 + 1;
-        colors = [0] * colorsz
-        for i in range(0, num_bytes, 3):
-            r = cmap[i] // 2 & 0x70
-            g = cmap[i+1] // 32
-            b = cmap[i+2] // 32
-            colors[i // 3 * 2] = r + b
-            colors[i // 3 * 2 + 1] = g
-            print("%i: %i, %i, %i" % (i // 3, r // 16, g, b))
+    palette = [0] * (2 * MAX_COLORS)
 
-        if image_enc == 'bin':
-            encoded = struct.pack('<BHHH{}B'.format(colorsz), FILE_PREFIX, PALETTE_OFFSET,
-                    PALETTE_OFFSET + len(colors), 0, *colors[0:colorsz])
-            file = open(os.path.join(folder, '%s.PAL' % filename), "wb")
-            file.write(encoded)
-            file.close()
+    for color, index in cfq.items():
+        r, g, b = color
+        palette[index * 2] = 16 * r + b
+        palette[index * 2 + 1] = g
+        print("palette[%i] = (%i, %i, %i)" % (index, r, g, b))
+
+    if exp_pal:
+        encoded = struct.pack('<BHHH{}B'.format(len(palette)), BIN_PREFIX, PALETTE_OFFSET,
+                PALETTE_OFFSET + len(palette), 0, *palette[0:len(palette)])
+        file = open(os.path.join(folder, '%s.PAL' % filename), "wb")
+        file.write(encoded)
+        file.close()
 
     pixels = [0] * (MAX_WIDTH // 2) * MAX_HEIGHT
     step = 1.0 / height
     percent = 0.0
+    printed = {}
 
     for y in range(0, height):
         for x in range(0, width):
-            _, indexed = gimpfu.pdb.gimp_drawable_get_pixel(drawable, x, y)
+            _, (indexed,) = gimpfu.pdb.gimp_drawable_get_pixel(drawable, x, y)
+
+            if printed.get(indexed) == None:
+                print("* replaced %i by %i" % (indexed, crep[indexed]))
+                printed[indexed] = True
+
+            indexed = crep[indexed]
             pos = x // 2 + y * 128
-            pixels[pos] |= indexed[0] if x % 2 else indexed[0] << 4;
+            pixels[pos] |= indexed if x % 2 else indexed << 4;
 
         percent += step
         gimpfu.pdb.gimp_progress_update(percent)
 
     if image_enc == 'bin':
-        encoded = struct.pack('<BHHH{}B'.format(len(pixels)), FILE_PREFIX, 0, len(pixels), 0, *pixels)
+        encoded = struct.pack('<BHHH{}B'.format(len(pixels)), BIN_PREFIX, 0, len(pixels), 0, *pixels)
     else:
         return
 
     file = open(os.path.join(folder, '%s.SC5' % filename), "wb")
     file.write(encoded)
     file.close()
+
+
+def reduce_colors(img):
+    num_bytes, cmap = gimpfu.pdb.gimp_image_get_cmap(img)
+    cfq = {}
+    crep = {}
+
+    for i in range(0, num_bytes, 3):
+        print("%i: checking color %i..." % (i, i // 3))
+        r = round(float(cmap[i]) / 0xff * 7) # // 32
+        g = round(float(cmap[i + 1]) / 0xff * 7) # // 32
+        b = round(float(cmap[i + 2]) / 0xff * 7) # // 32
+
+        if cfq.get((r, g, b)) == None:
+            cfq[(r, g, b)] = len(cfq)
+
+        crep[i // 3] = cfq[(r, g, b)]
+        print("* replacing index %i (#%02x%02x%02x) by %i (#%x%x%x)" % (i // 3, cmap[i], cmap[i + 1],
+            cmap[i + 2], cfq[(r, g, b)], r, g, b))
+
+    return cfq, crep
 
 
 gimpfu.register("msx_gr4_exporter",
@@ -115,8 +140,9 @@ gimpfu.register("msx_gr4_exporter",
                 "INDEXED", [
                     (gimpfu.PF_STRING, "filename", "File name", DEFAULT_FILENAME),
                     (gimpfu.PF_DIRNAME, "folder", "Output Folder", DEFAULT_OUTPUT_DIR),
-                    (gimpfu.PF_BOOL, "layers2pages", "Convert layers into pages", False),
-                    (gimpfu.PF_BOOL, "palette", "Export palette", True),
+                    #(gimpfu.PF_BOOL, "layers2pages", "Convert layers into pages", False),
+                    (gimpfu.PF_BOOL, "force_0_black", "Force black as color 0", False),
+                    (gimpfu.PF_BOOL, "exp_pal", "Export palette", True),
                     (gimpfu.PF_RADIO, "image-enc", "Image Encoding", DEFAULT_OUTPUT_FMT, (("BIN", "bin"),
                         ("RLE", "rle"), ("aPLib", "aplib"))),
                 ], 
